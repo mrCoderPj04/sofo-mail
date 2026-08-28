33bbcad642cca0652099de380f02b4cdc21d38f9
mrCoderPj04 <pjsofonic@gmail.com>
2026-08-28 13:42:29 +0530

    feat(env): set live production backend API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: (typeof window !== 'undefined' && (window as any).__SOFOMAIL_API_URL__) || 'http://localhost:8080/api/v1',
+  apiBaseUrl: (typeof window !== 'undefined' && (window as any).__SOFOMAIL_API_URL__) || 'https://pjsofonic-mail.onrender.com/api/v1',
   emsBackendUrl: 'https://erp-backend-1-02lc.onrender.com'
 };
